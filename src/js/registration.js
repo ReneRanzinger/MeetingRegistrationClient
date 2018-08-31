ed64fe084b6b70af6ea35512f9582aa0b3995b85
vinamrjain <er.vinamrajain@gmail.com>
2018-08-31 15:41:21 -0400

    temporary functional web page with web service calls

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,33 +1,36 @@
 var conferenceCode;
-var regfeeobject;
-var selectedRegFee;
-var titleselected;
-var orgtypeselected;
-var dietselected;
+//var orgtypeselected;
 
 
 function RegistrationSuccess() {
-    getselectedtitlevalue();
-    getSelectedRegistrationFee();
-    getselectedorgtypevalue();
-    getselecteddietvalue()
+  //  getselectedorgtypevalue();
+    
 
-    var title = titleselected;
+    var title_ddl = document.getElementById("title");    
+    var title = title_ddl.options[title_ddl.selectedIndex].value;
     var first_name = $("#first_name").val();
     var middle_name = $("#middle_name").val();
     var last_name = $("#last_name").val();
     var phone_num = $("#phone_num").val();
     var email = $("#email").val();
-    var re_enter_email = $("#re_enter_email").val();
     var address = $("#address").val();
     var profession = $("#profession").val();
-    var registration_fee = regfeeobject;
     var promo_code = $("#promo_code").val();
     var department = $("#department").val();
     var organization = $("#organization").val();
-    var organization_type = orgtypeselected;
-    var diet = dietselected;
+    //var organization_type = orgtypeselected;
+    var diet_ddl = document.getElementById("diet");
+    var diet = diet_ddl.options[diet_ddl.selectedIndex].value;
     var comments = $("#comments").val();
+
+    var selectedFee_ddl = document.getElementById("registrationfee");
+    var selectedFee = selectedFee_ddl.options[selectedFee_ddl.selectedIndex].value;
+    var FeeSplit = selectedFee.split(" ");
+    var conregfee = '';
+    for (i = 0; i < FeeSplit.length - 1; i++) {
+        conregfee = conregfee + FeeSplit[i] + " ";
+    }
+    var FeeObject  = { "name": conregfee, "amount": parseFloat(FeeSplit[FeeSplit.length - 1])};
 
     var form_object = {
         conferenceCode: conferenceCode,
@@ -42,10 +45,11 @@ function RegistrationSuccess() {
         phone: phone_num,
         profession: profession,
         promotionCode: promo_code,
-        fee: regfeeobject,
+        fee: FeeObject,
         comment: comments,
         diet: diet
     };
+    debugger;
     $.ajax({
         type: 'post',
         url: "http://localhost:8080/registration/register",
@@ -65,32 +69,10 @@ function RegistrationSuccess() {
     });
 }
 
-function getselectedtitlevalue() {
-    var titleddl = document.getElementById("title");
-    titleselected = titleddl.options[titleddl.selectedIndex].value;
-}
-
-function getselectedorgtypevalue() {
-    var orgtypeddl = document.getElementById("organizationtype");
-    orgtypeselected = orgtypeddl.options[orgtypeddl.selectedIndex].value;
-}
-
-function getselecteddietvalue() {
-    var dietddl = document.getElementById("diet");
-    dietselected = dietddl.options[dietddl.selectedIndex].value;
-}
-
-function getSelectedRegistrationFee() {
-    var regfeeddl = document.getElementById("registrationfee");
-    selectedRegFee = regfeeddl.options[regfeeddl.selectedIndex].value;
-    var selectedRegFeeSplit = selectedRegFee.split(" ");
-    var conregfee = '';
-    for (i = 0; i < selectedRegFeeSplit.length - 1; i++) {
-        conregfee = conregfee + selectedRegFeeSplit[i] + " ";
-    }
-    regfeeobject = { "name": conregfee, "amount": parseInt(selectedRegFeeSplit[selectedRegFeeSplit.length - 1] + 0.0) };
-
-}
+// function getselectedorgtypevalue() {
+//     var orgtypeddl = document.getElementById("organizationtype");
+//     orgtypeselected = orgtypeddl.options[orgtypeddl.selectedIndex].value;
+// }
 
 function WhichDivToHide() {
     $.ajax({
@@ -101,72 +83,42 @@ function WhichDivToHide() {
             HiddingDiv(data);
         },
         error: function(response) {
-            hideAllFirst();
+           // hideAllFirst();
             console.log(response);
             alert('fail');
         }
     });
 }
 
-function hideAllFirst() {
-    var x = document.getElementById("registrationonline");
-    var y = document.getElementById("registrationnotstarted");
-    var z = document.getElementById("registrationover");
-    x.style.display = "none";
-    y.style.display = "none";
-    z.style.display = "none";
-}
+
 
 function HiddingDiv(data) {
-    var statuscode = data['statusCode'];
+    var statuscode =0; //data['statusCode'];
     conferenceCode = data['conferenceCode'];
     configureRegistrationDropDown(data);
     if (statuscode == -1) {
-        var x = document.getElementById("registrationonline");
-        var y = document.getElementById("registrationnotstarted");
         var z = document.getElementById("registrationover");
-        x.style.display = "none";
-        y.style.display = "none";
         z.style.display = "block";
-        alert('sucess');
     }
     if (statuscode == 0) {
-
-        var x = document.getElementById("registrationover");
-        var y = document.getElementById("registrationnotstarted");
         var z = document.getElementById("registrationonline");
-        x.style.display = "none";
-        y.style.display = "none";
         z.style.display = "block";
-        alert('sucess');
     }
     if (statuscode == 1) {
-        var x = document.getElementById("registrationonline");
-        var y = document.getElementById("registrationover");
         var z = document.getElementById("registrationnotstarted");
-        x.style.display = "none";
-        y.style.display = "none";
         z.style.display = "block";
-        alert('sucess');
     }
-}
-
-function ChooseFile() {
-    var chooseFileButton = document.getElementById("choose_file_button");
-    chooseFileButton.setAttribute("type", "file");
-    document.body.appendChild(x);
 }
 
 function configureRegistrationDropDown(data) {
-    var regddl = document.getElementById("registrationfee");
+    var fee_ddl = document.getElementById("registrationfee");
     for (i = 0; i < data.fees.length; i++) {
         var feeObj = data.fees[i];
         var regNameString = feeObj["name"];
-        var regFeeInt = feeObj["amount"];
-        var conregoption = regNameString + " " + regFeeInt;
+        var regFeeInt = feeObj["amount"]; 
         var option = document.createElement("option");
-        option.text = conregoption;
-        option.value = conregoption;
-        regddl.add(option);
+        option.text = regNameString + " - $" + regFeeInt;;
+        option.value = regNameString + " " + regFeeInt;
+        fee_ddl.add(option);
     }
 }
